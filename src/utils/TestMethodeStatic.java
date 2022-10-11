8fa97788ec709752dd4f52f3e818057375ee9fce
Christian44444 <c.michaud@laposte.net>
2022-10-11 12:36:46 +0200

    Utilisation de méthode static

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String chaine = "12";
+		int chaineEnEntier = java.lang.Integer.decode(chaine);
+		System.out.println(chaineEnEntier);
+		int a = -8;
+		int b = -3;
+		System.out.println(java.lang.Integer.max(a, b));
+	}
+
+}
