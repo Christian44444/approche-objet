487d93d87564f78690df599ab63c5afffcd61668
Christian44444 <c.michaud@laposte.net>
2022-10-18 09:49:16 +0200

    Fin manip fichiers

--- src/fichier/LectureFichier.java
+++ src/fichier/LectureFichier.java
@@ -21,11 +21,18 @@ public class LectureFichier {
 		// ne pas traiter la première ligne
 		boolean first = true;
 		
+		String enteteFichier = "";
+		
 		for (String l: lignes) {
+			String[] ligneDecoupee = l.split(";");
 			if (first) {
 				first = false;
+				enteteFichier = ligneDecoupee[6] + ";" +
+								ligneDecoupee[2] + ";" +
+								ligneDecoupee[1] + ";" +
+								ligneDecoupee[9] + ";";
 			} else {
-				String[] ligneDecoupee = l.split(";");				
+								
 				Villes v = new Villes(ligneDecoupee[6], ligneDecoupee[2], ligneDecoupee[1], Integer.parseInt(ligneDecoupee[9].replaceAll("\\s",""))); 
 				mesVilles.add(v);
 			}
@@ -33,6 +40,25 @@ public class LectureFichier {
 		for (Villes v : mesVilles) {
 			System.out.println(v);
 		}
- 	}
-
+		// Création du fichier de sorti
+		// Constitution de la liste des lignes de sortie
+		List<String> lignesSorties = new ArrayList<>();
+		
+		// Ecriture de la ligne d'entête
+		lignesSorties.add(enteteFichier);
+			
+		// Ecriture des lignes de données
+		for (Villes v : mesVilles) {
+			if (v.getPopTotale() > 25000) {
+				lignesSorties.add(v.getNom() + ";" + v.getCodeDep() + ";" + v.getNomRegion() + ";" + v.getPopTotale().toString() + ";");	
+			}
+		}
+		
+		String monFichierSorti = "C:\\Users\\cmich\\Documents\\Christian\\Pole Emploi\\Diginamic\\Cours\\7 - Langage Java - Approche Objet\\J5 & J6 - Collections et fichiers\\TP\\resultat.csv";
+		chemin = Paths.get(monFichierSorti);
+		/* Ecriture des lignes */
+		Files.write(chemin, lignesSorties);
+		
+	}
 }
+
