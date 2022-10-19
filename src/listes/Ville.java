26247db98542155da672db09d359ff94409026e2
Christian44444 <c.michaud@laposte.net>
2022-10-19 17:46:59 +0200

    Javadoc dans Classe Ville

--- src/listes/Ville.java
+++ src/listes/Ville.java
@@ -1,11 +1,20 @@
 package listes;
-
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
-
+/**
+ * Représente le concept de Ville avec un nom et un nombre d'habitant.
+ * @author cmich
+ *
+ */
 public class Ville implements Comparable<Ville>{
+	/** Nom de la ville	 */
 	private String nom;
+	/** Nombre d'habitants	 */
 	private Integer nbHab;
-	
+	/**
+	 * Constructeur
+	 * 
+	 * @param nom nom de la ville
+	 * @param nbHab nombre d'habitants
+	 */
 	public Ville(String nom, Integer nbHab) {
 		super();
 		this.nom = nom;
@@ -20,10 +29,10 @@ public class Ville implements Comparable<Ville>{
 		// Là on a une ville et elle est valide
 		// On doit caster obj pour accéder à ses propriétés
 		Ville autre = (Ville)obj;
-		
+		// Attention ne gère pas les attributs null de l'objet qui porte le .<méthode()> 
 		return autre.getNom().equals(this.getNom()) && (autre.getNbHab().equals(this.getNbHab()));
 		
-		// Le test d'égalité est fait dans la classe TestEquals()
+		// Le test d'égalité est fait dans la classe TestVille()
 	}
 	
 	@Override
@@ -48,27 +57,32 @@ public class Ville implements Comparable<Ville>{
 
 
 
+	/**
+	 * @return nom
+	 */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	 * @param nom
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
+	/**
+	 * @return nombre d'habitants
+	 */
 	public Integer getNbHab() {
 		return nbHab;
 	}
 
+	/**
+	 * @param nbHab
+	 */
 	public void setNbHab(Integer nbHab) {
 		this.nbHab = nbHab;
 	}
-
-
-
-	
-	
-	
-		
 
 }
