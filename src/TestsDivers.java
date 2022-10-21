dcee82e0d317687b9e0a275fdc6c1b0933363038
Christian44444 <c.michaud@laposte.net>
2022-10-21 17:31:11 +0200

    test compareTo

--- src/TestsDivers.java
+++ src/TestsDivers.java
@@ -1,12 +1,14 @@
+import fr.diginamic.testenumeration.Continent;
+import listes.Ville;
 
 public class TestsDivers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		"Jojo".compareTo("jojo");
-
+		Ville v = new Ville(Continent.AFRIQUE, "Naerobi", 4500000);
+		Ville ville = new Ville(Continent.AFRIQUE, "Naerobi", 4500000);
+		System.out.println(v.compareTo(ville));
+		
 	}
-	
-	
 
 }
